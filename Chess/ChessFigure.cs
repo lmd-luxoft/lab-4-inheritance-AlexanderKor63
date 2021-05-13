4d950cd155da1c33ae5d753bd9bcca8ec89c63c3
korablin <korablin@specialist.ru>
2021-05-13 14:07:51 +0300

    удалено дублирование в иерархии классов шахматных фигур, поднятие методов

--- Chess/ChessFigure.cs
+++ Chess/ChessFigure.cs
@@ -7,58 +7,47 @@ namespace Chess
     {
         internal ROOK(string current) : base(current) { }
 
-        internal override bool isMove(string nextCoord) {
-            if ((nextCoord[0] != currentCoord[0]) && (nextCoord[1] != currentCoord[1]) || ((nextCoord[0] == currentCoord[0]) && (nextCoord[1] == currentCoord[1])))
-                return false;
-            else
-                return true;
+        internal override bool canMove(string nextCoord) {
+            return !((nextCoord[0] != currentCoord[0]) && (nextCoord[1] != currentCoord[1]) ||
+                ((nextCoord[0] == currentCoord[0]) && (nextCoord[1] == currentCoord[1])));
         }
     }
+
     class KNIGHT : ChessFigure
     {
         internal KNIGHT(string current) : base(current) { }
 
-        internal override bool isMove(string nextCoord) {
-            int dx, dy;
-            dx = Math.Abs(nextCoord[0] - currentCoord[0]);
-            dy = Math.Abs(nextCoord[1] - currentCoord[1]);
-            if (!(Math.Abs(nextCoord[0] - currentCoord[0]) == 1 && Math.Abs(nextCoord[1] - currentCoord[1]) == 2 || Math.Abs(nextCoord[0] - currentCoord[0]) == 2 && Math.Abs(nextCoord[1] - currentCoord[1]) == 1))
-                return false;
-            else
-                return true;
+        internal override bool canMove(string nextCoord) {
+            return (Math.Abs(nextCoord[0] - currentCoord[0]) == 1 && Math.Abs(nextCoord[1] - currentCoord[1]) == 2
+                || Math.Abs(nextCoord[0] - currentCoord[0]) == 2 && Math.Abs(nextCoord[1] - currentCoord[1]) == 1);
         }
     }
+
     class BISHOP : ChessFigure
     {
         internal BISHOP(string current) : base(current) { }
 
-        internal override bool isMove(string nextCoord) {
-            if (!(Math.Abs(nextCoord[0] - currentCoord[0]) == Math.Abs(nextCoord[1] - currentCoord[1])))
-                return false;
-            else
-                return true;
+        internal override bool canMove(string nextCoord) {
+            return (Math.Abs(nextCoord[0] - currentCoord[0]) == Math.Abs(nextCoord[1] - currentCoord[1]));
         }
     }
+
     class PAWN   : ChessFigure
     {
         internal PAWN(string current) : base(current)  { }
 
-        internal override bool isMove(string nextCoord) {
-            if (nextCoord[0] != currentCoord[0] || nextCoord[1] <= currentCoord[1] || (nextCoord[1] - currentCoord[1] != 1 && (currentCoord[1] != '2' || nextCoord[1] != '4')))
-                return false;
-            else
-                return true;
+        internal override bool canMove(string nextCoord) {
+            return !(nextCoord[0] != currentCoord[0] || nextCoord[1] <= currentCoord[1] ||
+                (nextCoord[1] - currentCoord[1] != 1 && (currentCoord[1] != '2' || nextCoord[1] != '4')));
         }
     }
+
     class KING : ChessFigure
     {
         internal KING(string current) : base(current)  { }
 
-        internal override bool isMove(string nextCoord) {
-            if (!(Math.Abs(nextCoord[0] - currentCoord[0]) <= 1 && Math.Abs(nextCoord[1] - currentCoord[1]) <= 1))
-                return false;
-            else
-                return true;
+        internal override bool canMove(string nextCoord) {
+            return Math.Abs(nextCoord[0] - currentCoord[0]) <= 1 && Math.Abs(nextCoord[1] - currentCoord[1]) <= 1;
         }
     }
 
@@ -66,12 +55,9 @@ namespace Chess
     {
         internal QUEEN(string current) : base(current) { }
 
-        internal override bool isMove(string nextCoord) {
-            if (!(Math.Abs(nextCoord[0] - currentCoord[0]) == Math.Abs(nextCoord[1] - currentCoord[1])
-                || nextCoord[0] == currentCoord[0] || nextCoord[1] == currentCoord[1]))
-                return false;
-            else
-                return true;
+        internal override bool canMove(string nextCoord) {
+            return Math.Abs(nextCoord[0] - currentCoord[0]) == Math.Abs(nextCoord[1] - currentCoord[1])
+                || nextCoord[0] == currentCoord[0] || nextCoord[1] == currentCoord[1];
         }
     }
 
@@ -79,10 +65,14 @@ namespace Chess
         protected string currentCoord;
         internal ChessFigure(string current) { currentCoord = current; }
 
-        internal bool Move(string nextCoord) { return isField(nextCoord) ? isMove(nextCoord) : false; }
+        internal bool Move(string nextCoord) {
+            return isField(nextCoord) ?
+                 (!canMove(nextCoord) ? false : true) : false;
+        }
 
-        internal abstract bool isMove(string nextCoord);
-        internal bool isField(string nextCoord) {
+        internal abstract bool canMove(string nextCoord);
+
+        protected bool isField(string nextCoord) {
             return (nextCoord[0] >= 'A' && nextCoord[0] <= 'H' && nextCoord[1] >= '1' && nextCoord[1] <= '8');
         }
     }
